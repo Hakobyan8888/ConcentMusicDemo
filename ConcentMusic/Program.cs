dded6f0abef9df14a919f3c83ecaeb7db6b7e10e
Arthur <arturh376@gmail.com>
2019-04-11 16:50:12 +0400

    Add project files.

---
+++ ConcentMusic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConcentMusic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
